384955b7d8fd7a25aa7b01f0adae3665758a0b04
Andrii Romanenko <andrejjrom@gmail.com>
2022-07-29 14:10:25 +0300

    fix

---
+++ terraform/glue/modules/test/iam.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_role" "crawler_role" {
+  name = "${var.prefix}-crawler-role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "glue.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
